759eef50849693768e277d26f27023f6aef16cac
= <=>
2024-06-07 14:26:57 +0100

    cleaned up vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     rollupOptions: {
-      //   input: {
-      //     index: resolve(__dirname, "index.html"),
-      //     "blog-article": resolve(__dirname, "blog-article.html"),
-      //     blog: resolve(__dirname, "blog.html"),
-      //     about: resolve(__dirname, "about.html"),
-      //     contact: resolve(__dirname, "contact.html"),
-      //   },
       input: [
         "index.html",
         "blog.html",
